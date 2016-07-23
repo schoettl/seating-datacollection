ff768c255953dd868158ca685ece978301f0727e
Jakob Schöttl <jschoett@gmail.com>
2016-07-23 11:23:33 +0200

    Reset database

--- SeatingDataCollection/app/src/main/java/edu/hm/cs/vadere/seating/datacollection/SeatingDataCollectionApp.java
+++ SeatingDataCollection/app/src/main/java/edu/hm/cs/vadere/seating/datacollection/SeatingDataCollectionApp.java
@@ -1,14 +1,23 @@
 package edu.hm.cs.vadere.seating.datacollection;
 
+import android.util.Log;
+
 import com.orm.SugarApp;
 
 public class SeatingDataCollectionApp extends SugarApp {
 
     public static final String DATABASE_NAME = "data.db";
-    public static final int DATABASE_VERSION = 5;
+    public static final int DATABASE_VERSION = 1;
+    private static final String TAG = "SeatingDCApp";
 
     @Override
     public void onCreate() {
+        deleteDatabase();
         super.onCreate();
+    }
+
+    private void deleteDatabase() {
+        Log.i(TAG, "deleting database");
+        deleteDatabase(DATABASE_NAME);
     }
 }
