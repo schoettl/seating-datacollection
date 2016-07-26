037b3a8a2be5aa60221001b116088d40d0a339dd
Jakob Schöttl <jschoett@gmail.com>
2016-07-26 19:02:28 +0200

    Fix bug on transferring groups

--- SeatingDataCollection/app/src/main/java/edu/hm/cs/vadere/seating/datacollection/model/MGroup.java
+++ SeatingDataCollection/app/src/main/java/edu/hm/cs/vadere/seating/datacollection/model/MGroup.java
@@ -2,8 +2,10 @@ package edu.hm.cs.vadere.seating.datacollection.model;
 
 import com.orm.SugarRecord;
 
+import java.io.Serializable;
+
 /** GROUP is a reserved word in SQL. Therefore, "M" for model or mates. */
-public class MGroup extends SugarRecord {
+public class MGroup extends SugarRecord implements Serializable {
     @Override
     public String toString() {
         return "group " + getId();
--- SeatingDataCollection/app/src/main/java/edu/hm/cs/vadere/seating/datacollection/seats/SeatsState.java
+++ SeatingDataCollection/app/src/main/java/edu/hm/cs/vadere/seating/datacollection/seats/SeatsState.java
@@ -6,6 +6,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.hm.cs.vadere.seating.datacollection.model.MGroup;
+import edu.hm.cs.vadere.seating.datacollection.model.Person;
 import edu.hm.cs.vadere.seating.datacollection.model.Seat;
 import edu.hm.cs.vadere.seating.datacollection.model.SeatTaker;
 
@@ -35,19 +37,45 @@ public class SeatsState implements Serializable {
     public class SerializableSeatTaker implements Serializable {
         private Long idIfPresent;
         private SeatTaker seatTaker;
-
-        public SeatTaker getSeatTaker() {
-            SeatTaker result = seatTaker;
-            if (result instanceof SugarRecord)
-                ((SugarRecord) result).setId(idIfPresent);
-            return result;
-        }
+        private Long groupIdIfPresent;
 
         public SerializableSeatTaker(SeatTaker seatTaker) {
             this.seatTaker = seatTaker;
             this.idIfPresent = null;
             if (seatTaker instanceof SugarRecord)
                 this.idIfPresent = ((SugarRecord) seatTaker).getId();
+
+            this.groupIdIfPresent = maybeGetGroupId(seatTaker);
+        }
+
+        public SeatTaker getSeatTaker() {
+            SeatTaker result = seatTaker;
+            if (result instanceof SugarRecord)
+                ((SugarRecord) result).setId(idIfPresent);
+
+            maybeSetGroupId(result, groupIdIfPresent);
+
+            return result;
+        }
+
+        private Long maybeGetGroupId(SeatTaker seatTaker) {
+            MGroup g = getGroupIfPresent(seatTaker);
+            if (g != null)
+                return g.getId();
+            return null;
+        }
+
+        private void maybeSetGroupId(SeatTaker seatTaker, Long groupIdOrNull) {
+            MGroup g = getGroupIfPresent(seatTaker);
+            if (g != null)
+                g.setId(groupIdOrNull);
+        }
+
+        private MGroup getGroupIfPresent(SeatTaker seatTaker) {
+            if (seatTaker instanceof Person)
+                return ((Person) seatTaker).getGroup();
+            return null;
         }
     }
+
 }
