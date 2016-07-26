5263c785e6acd786d86dc7e763687b45e6789c17
Jakob Schöttl <jschoett@gmail.com>
2016-07-26 11:25:54 +0200

    Stop using serialized person in dialog

--- SeatingDataCollection/app/src/main/java/edu/hm/cs/vadere/seating/datacollection/PersonDialogFragment.java
+++ SeatingDataCollection/app/src/main/java/edu/hm/cs/vadere/seating/datacollection/PersonDialogFragment.java
@@ -18,13 +18,19 @@ import edu.hm.cs.vadere.seating.datacollection.model.Person;
 
 public class PersonDialogFragment extends DialogFragment {
     private static final String TAG = "PersonDialog";
-    private static final String ARG_PERSON_KEY = "bad8cbcf94421963bf863495daba5afd4fe644d4";
+    private static final String ARG_PERSON_ID_KEY = "6f962b504bd465cc949cfef640653d173daf0c4c";
+    private static final String ARG_PERSON_GENDER_KEY = "bad8cbcf94421963bf863495daba5afd4fe644d4";
+    private static final String ARG_PERSON_AGE_GROUP_KEY = "77640998084251deb1926451b2bb2bc5763a9143";
 
-    private Person person;
+    private long personId;
+    private Gender personGender;
+    private AgeGroup personAgeGroup;
 
     public static DialogFragment newInstance(Person person) {
         Bundle args = new Bundle();
-        args.putSerializable(ARG_PERSON_KEY, person);
+        args.putSerializable(ARG_PERSON_ID_KEY, person.getId());
+        args.putSerializable(ARG_PERSON_GENDER_KEY, person.getGender());
+        args.putSerializable(ARG_PERSON_AGE_GROUP_KEY, person.getAgeGroup());
 
         DialogFragment dialog = new PersonDialogFragment();
         dialog.setArguments(args);
@@ -36,7 +42,9 @@ public class PersonDialogFragment extends DialogFragment {
         super.onCreateDialog(savedInstanceState);
         Log.d(TAG, "on create dialog");
 
-        person = (Person) getArguments().get(ARG_PERSON_KEY);
+        personId = (long) getArguments().get(ARG_PERSON_ID_KEY);
+        personGender = (Gender) getArguments().get(ARG_PERSON_GENDER_KEY);
+        personAgeGroup = (AgeGroup) getArguments().get(ARG_PERSON_AGE_GROUP_KEY);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setMessage(R.string.dialog_person_message);
@@ -47,7 +55,8 @@ public class PersonDialogFragment extends DialogFragment {
                 Spinner spinnerGender = (Spinner) d.findViewById(R.id.spinnerGender);
                 Spinner spinnerAgeGroup = (Spinner) d.findViewById(R.id.spinnerAgeGroup);
 
-                Log.d(TAG, "update and save person: " + person);
+                Log.d(TAG, "updating person with id " + personId);
+                Person person = Person.findById(Person.class, personId);
                 person.setGender((Gender) spinnerGender.getSelectedItem());
                 person.setAgeGroup((AgeGroup) spinnerAgeGroup.getSelectedItem());
                 person.save();
@@ -62,6 +71,9 @@ public class PersonDialogFragment extends DialogFragment {
             }
         });
 
+        // This line does not really inflate the view, so setInitialValues does not work
+        //builder.setView(R.layout.dialog_person);
+
         // This line causes a recursion:
         //LayoutInflater inflater = getLayoutInflater(savedInstanceState);
         LayoutInflater inflater = getActivity().getLayoutInflater();
@@ -73,15 +85,15 @@ public class PersonDialogFragment extends DialogFragment {
     }
 
     private void setInitialValues(View view) {
-        Log.d(TAG, "sitting initial values");
+        Log.d(TAG, "setting initial values");
 
         Spinner spinnerGender = getSpinner(view, R.id.spinnerGender);
         setSimpleSpinnerAdapter(spinnerGender, Gender.values());
-        spinnerGender.setSelection(person.getGender().ordinal());
+        spinnerGender.setSelection(personGender.ordinal());
 
         Spinner spinnerAgeGroup = getSpinner(view, R.id.spinnerAgeGroup);
         setSimpleSpinnerAdapter(spinnerAgeGroup, AgeGroup.values());
-        spinnerAgeGroup.setSelection(person.getAgeGroup().ordinal());
+        spinnerAgeGroup.setSelection(personAgeGroup.ordinal());
     }
 
     private Spinner getSpinner(View view, int spinnerAgeGroup) {
