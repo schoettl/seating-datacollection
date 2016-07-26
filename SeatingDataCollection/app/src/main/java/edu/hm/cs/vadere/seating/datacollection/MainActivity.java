eda7ef9d467469c7b3d4353975496d7439d2c3cc
Jakob Schöttl <jschoett@gmail.com>
2016-07-26 23:05:23 +0200

    Create a main activity

---
+++ SeatingDataCollection/app/src/main/java/edu/hm/cs/vadere/seating/datacollection/MainActivity.java
@@ -0,0 +1,80 @@
+package edu.hm.cs.vadere.seating.datacollection;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final int EXPORT_DATA_REQUEST_CODE = 1;
+    private static final String[] REQUIRED_PERMISSIONS = { Manifest.permission.WRITE_EXTERNAL_STORAGE };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        UiHelper.setToolbar(this);
+    }
+
+    public void startNewSurvey(View view) {
+        Intent intent = new Intent(this, StartSurveyActivity.class);
+        startActivity(intent);
+    }
+
+    public void exportData(View view) {
+        Log.i(TAG, "requesting permissions for export first");
+        if (!requestPermissions()) {
+            return;
+        }
+
+        Log.i(TAG, "starting export");
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        AsyncTask<Void, ?, ?> task = new DatabaseExportTask(getBaseContext(), progressDialog);
+        task.execute();
+    }
+
+    private boolean requestPermissions() {
+        int permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                // TODO show explanation instead of just retrying:
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, EXPORT_DATA_REQUEST_CODE);
+            } else {
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, EXPORT_DATA_REQUEST_CODE);
+            }
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case EXPORT_DATA_REQUEST_CODE:
+                if (areAllPermissionsGranted(grantResults)) {
+                    Log.i(TAG, "permissions for export granted");
+                    exportData(null);
+                } else {
+                    Log.i(TAG, "permissions for export denied");
+                }
+                break;
+        }
+    }
+
+    private boolean areAllPermissionsGranted(@NonNull int[] grantResults) {
+        for (int result : grantResults)
+            if (result != PackageManager.PERMISSION_GRANTED)
+                return false;
+        return true;
+    }
+
+}
--- SeatingDataCollection/app/src/main/java/edu/hm/cs/vadere/seating/datacollection/StartSurveyActivity.java
+++ SeatingDataCollection/app/src/main/java/edu/hm/cs/vadere/seating/datacollection/StartSurveyActivity.java
@@ -1,15 +1,8 @@
 package edu.hm.cs.vadere.seating.datacollection;
 
-import android.Manifest;
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -17,11 +10,9 @@ import edu.hm.cs.vadere.seating.datacollection.model.Survey;
 
 public class StartSurveyActivity extends AppCompatActivity {
 
+    private static final String TAG = "StartSurveyActivity";
     public static final String EXTRA_SURVEY_ID_KEY = "44cb788eb14b3d9a670962249fb0da402999aa72";
 
-    private static final int EXPORT_DATA_REQUEST_CODE = 1;
-    private static final String[] REQUIRED_PERMISSIONS = { Manifest.permission.WRITE_EXTERNAL_STORAGE };
-    private static final String TAG = "StartSurveyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,53 +26,6 @@ public class StartSurveyActivity extends AppCompatActivity {
         super.onStart();
         String todayStr = Utils.getTodaysDateIsoFormat();
         getEditTextById(R.id.editTextDate).setText(todayStr);
-    }
-
-    private boolean requestPermissions() {
-        int permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                // TODO show explanation instead of just retrying:
-                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, EXPORT_DATA_REQUEST_CODE);
-            } else {
-                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, EXPORT_DATA_REQUEST_CODE);
-            }
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case EXPORT_DATA_REQUEST_CODE:
-                if (areAllPermissionsGranted(grantResults)) {
-                    Log.i(TAG, "permissions for export granted");
-                    exportData(null);
-                } else {
-                    Log.i(TAG, "permissions for export denied");
-                }
-                break;
-        }
-    }
-
-    private boolean areAllPermissionsGranted(@NonNull int[] grantResults) {
-        for (int result : grantResults)
-            if (result != PackageManager.PERMISSION_GRANTED)
-                return false;
-        return true;
-    }
-
-    public void exportData(View view) {
-        Log.i(TAG, "requesting permissions for export first");
-        if (!requestPermissions()) {
-            return;
-        }
-
-        Log.i(TAG, "starting export");
-        ProgressDialog progressDialog = new ProgressDialog(this);
-        AsyncTask<Void, ?, ?> task = new DatabaseExportTask(getBaseContext(), progressDialog);
-        task.execute();
     }
 
     public void startSurvey(View view) {
