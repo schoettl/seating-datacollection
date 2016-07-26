e7553c92d04708295b546d629b2864f5686dba30
Jakob Schöttl <jschoett@gmail.com>
2016-07-26 19:02:42 +0200

    Implement count standing persons

--- SeatingDataCollection/app/src/main/java/edu/hm/cs/vadere/seating/datacollection/CollectDataActivity.java
+++ SeatingDataCollection/app/src/main/java/edu/hm/cs/vadere/seating/datacollection/CollectDataActivity.java
@@ -1,9 +1,13 @@
 package edu.hm.cs.vadere.seating.datacollection;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import edu.hm.cs.vadere.seating.datacollection.model.LogEventType;
 import edu.hm.cs.vadere.seating.datacollection.model.Survey;
@@ -51,9 +55,31 @@ public class CollectDataActivity extends AppCompatActivity {
             case R.id.action_train_starts:
                 logEventWriter.logTrainEvent(LogEventType.TRAIN_STARTS);
                 return true;
+            case R.id.action_count_standing_persons:
+                actionCountStandingPersons();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void actionCountStandingPersons() {
+        final EditText editTextCount = new EditText(this);
+        editTextCount.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.dialog_count_standing_persons);
+        builder.setView(editTextCount);
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                int count = Integer.parseInt(editTextCount.getText().toString());
+                // TODO catch exception
+                logEventWriter.logCountStandingPersons(count);
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, Utils.ON_CLICK_DO_NOTHING_LISTENER);
+        builder.show();
     }
 
     @Override
