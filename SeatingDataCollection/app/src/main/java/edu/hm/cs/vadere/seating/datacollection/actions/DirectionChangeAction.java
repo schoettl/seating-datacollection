8b5de9526984fa498d7cc50e5f354e534d649e37
Jakob Schöttl <jschoett@gmail.com>
2016-07-29 17:31:09 +0200

    Implement undo for direction change

--- SeatingDataCollection/app/src/main/java/edu/hm/cs/vadere/seating/datacollection/actions/DirectionChangeAction.java
+++ SeatingDataCollection/app/src/main/java/edu/hm/cs/vadere/seating/datacollection/actions/DirectionChangeAction.java
@@ -3,12 +3,19 @@ package edu.hm.cs.vadere.seating.datacollection.actions;
 import edu.hm.cs.vadere.seating.datacollection.model.LogEventType;
 
 public class DirectionChangeAction extends Action {
+    private long logEventId;
+
     protected DirectionChangeAction(ActionManager actionManager) {
         super(actionManager);
     }
 
     @Override
     public void perform() {
-        getLogEventWriter().logTrainEvent(LogEventType.DIRECTION_CHANGE);
+        logEventId = getLogEventWriter().logTrainEvent(LogEventType.DIRECTION_CHANGE);
+    }
+
+    @Override
+    public void undo() throws UnsupportedOperationException {
+        deleteLogEvent(logEventId);
     }
 }
