e7248645e5fc0b75bd9371895d012f1b91d3f317
Jakob Schöttl <jschoett@gmail.com>
2016-08-28 13:51:19 +0200

    Add age group for young children

--- SeatingDataCollection/app/src/main/java/edu/hm/cs/vadere/seating/datacollection/model/AgeGroup.java
+++ SeatingDataCollection/app/src/main/java/edu/hm/cs/vadere/seating/datacollection/model/AgeGroup.java
@@ -3,6 +3,7 @@ package edu.hm.cs.vadere.seating.datacollection.model;
 public enum AgeGroup {
     // NA instead of null, to allow selecting it in the UI
     NA,
+    YOUNG_CHILD,
     SCHOOLCHILD,
     YOUTHFUL,
     YOUNG_ADULT,
