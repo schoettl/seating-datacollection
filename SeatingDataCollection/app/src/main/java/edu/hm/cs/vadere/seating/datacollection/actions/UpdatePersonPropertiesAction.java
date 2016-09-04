e8a09561cdd9aa1f010c1bb190e214bfc60745bf
Jakob Schöttl <jschoett@gmail.com>
2016-09-04 10:33:33 +0200

    Use standard dialog with lists instead of custom dialog

--- SeatingDataCollection/app/src/main/java/edu/hm/cs/vadere/seating/datacollection/actions/UpdatePersonPropertiesAction.java
+++ SeatingDataCollection/app/src/main/java/edu/hm/cs/vadere/seating/datacollection/actions/UpdatePersonPropertiesAction.java
@@ -1,6 +1,9 @@
 package edu.hm.cs.vadere.seating.datacollection.actions;
 
-import edu.hm.cs.vadere.seating.datacollection.PersonDialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+
 import edu.hm.cs.vadere.seating.datacollection.R;
 import edu.hm.cs.vadere.seating.datacollection.UiHelper;
 import edu.hm.cs.vadere.seating.datacollection.model.AgeGroup;
@@ -17,16 +20,61 @@ public class UpdatePersonPropertiesAction extends Action {
 
     @Override
     public void perform() {
-        PersonDialogFragment dialog = PersonDialogFragment.newInstance(person);
-        dialog.setOkClickListener(new PersonDialogFragment.PositiveClickListener() {
+        AlertDialog ageGroupDialog = createAgeGroupDialog();
+        AlertDialog genderDialog = createGenderDialog(ageGroupDialog);
+        genderDialog.show(); // genderDialog starts ageGroupDialog
+    }
+
+    private AlertDialog createAgeGroupDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle(R.string.select_age_group);
+        builder.setSingleChoiceItems(getDialogItems(AgeGroup.values()), person.getAgeGroup().ordinal(), new DialogInterface.OnClickListener() {
             @Override
-            public void onPersonDialogOkClick(Gender gender, AgeGroup ageGroup) {
-                person.setGender(gender);
-                person.setAgeGroup(ageGroup);
-                person.save();
+            public void onClick(DialogInterface dialog, int which) {
+                if (which != -1) {
+                    person.setAgeGroup(AgeGroup.values()[which]);
+                    person.save();
+                }
+                dialog.dismiss();
             }
         });
-        dialog.show(getActionManager().getSeatsFragment().getActivity().getSupportFragmentManager(), PersonDialogFragment.FRAGMENT_TAG);
+        return builder.create();
+    }
+
+    private AlertDialog createGenderDialog(final AlertDialog ageGroupDialog) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle(R.string.select_gender);
+        builder.setSingleChoiceItems(getDialogItems(Gender.values()), person.getGender().ordinal(), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which != -1) {
+                    person.setGender(Gender.values()[which]);
+                    person.save();
+                }
+                dialog.dismiss();
+                ageGroupDialog.show();
+            }
+        });
+        return builder.create();
+    }
+
+    private <E> String[] getDialogItems(E[] values) {
+//        return new ArrayAdapter<>(getContext(), R.layout.item_spinner, values); // I want to use the standard layout!
+        // What I want to do: convert object array to string array
+        // In Haskell: map toString values
+        // In Java: http://stackoverflow.com/questions/1018750/how-to-convert-object-array-to-string-array-in-java
+//        return Arrays.copyOf(values, values.length, String[].class); // does not work on Android (ArrayStoreException)
+//        return Arrays.asList(values).toArray(new String[values.length]); // does not work on Android (ArrayStoreException)
+        // Solution:
+        String[] result = new String[values.length];
+        for (int i = 0; i < values.length; i++) {
+            result[i] = values[i].toString();
+        }
+        return result;
+    }
+
+    private Context getContext() {
+        return getActionManager().getSeatsFragment().getContext();
     }
 
     @Override
